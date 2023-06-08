f2ea7e530a4539df9f54f1cf787d68a7793e95e0
Sacr1fice <sanunkurov@gmail.com>
2023-06-08 12:54:31 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int popolnenie = 1100;
+
+        int bonus = 0;
+        if (popolnenie > 1000) {
+                bonus = popolnenie / 100;
+        }
+
+        int itog = balance + popolnenie + bonus;
+
+        System.out.println("Итоговый баланс: " + itog);
+        System.out.println("Количество бонусов: " + bonus);
+    }
+}
