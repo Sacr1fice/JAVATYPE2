7881c1108bb60a4b2f5584050a89053dd777f301
Sacr1fice <sanunkurov@gmail.com>
2023-06-13 11:55:27 +0300

    dorabotka

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100;
-        int popolnenie = 1100;
+        int replenishment = 1100;
 
         int bonus = 0;
-        if (popolnenie > 1000) {
-            bonus = popolnenie / 100;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
         }
 
-        int itog = balance + popolnenie + bonus;
+        int total = balance + replenishment + bonus;
 
-        System.out.println("Итоговый баланс: " + itog);
+        System.out.println("Итоговый баланс: " + total);
         System.out.println("Количество бонусов: " + bonus);
     }
 }
